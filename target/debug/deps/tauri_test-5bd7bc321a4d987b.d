47a61cde50bc591198ad352726f87f35c3b4f2c2
TzeYiing <ty@tzeyiing.com>
2026-04-23 03:46:08 +0800

    chore: initial scaffolding

---
+++ target/debug/deps/tauri_test-5bd7bc321a4d987b.d
@@ -0,0 +1,5 @@
+/Users/tzeyiing/projects/tauri-test/target/debug/deps/libtauri_test-5bd7bc321a4d987b.rmeta: src/main.rs
+
+/Users/tzeyiing/projects/tauri-test/target/debug/deps/tauri_test-5bd7bc321a4d987b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/tauri_test-eaff24684de32f5c.d
@@ -0,0 +1,5 @@
+/Users/tzeyiing/projects/tauri-test/target/debug/deps/libtauri_test-eaff24684de32f5c.rmeta: src/main.rs
+
+/Users/tzeyiing/projects/tauri-test/target/debug/deps/tauri_test-eaff24684de32f5c.d: src/main.rs
+
+src/main.rs:
